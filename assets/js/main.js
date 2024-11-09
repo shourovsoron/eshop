351ea5e08d84b78d530fb2d69adcfc681feedc08
Shourov Soron <shourovhossainsoron@gmail.com>
2024-11-09 15:59:26 +0600

    project Complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,13 @@
 let navToggler = document.querySelector(".navbar-toggler");
 let navMenubar = document.querySelector(".collapse.navbar-collapse");
-let collapsing = document.querySelector(".collapsing");
-let collapsed = document.querySelector(".collapsed");
 let showclass = document.querySelector(".show");
 
+document.addEventListener("DOMContentLoaded", function() {
+    const scrollSpy = new bootstrap.ScrollSpy(document.body, {
+      target: '#navbar-example',
+      offset: 60
+    });
+  });
 
 
 navMenubar.addEventListener("click", (e) => {
@@ -18,108 +22,52 @@ navMenubar.addEventListener("click", (e) => {
 
 
 
+let dayItem = document.querySelector("#days");
+let hourItem = document.querySelector("#hours");
+let minItem = document.querySelector("#min");
+let secItem = document.querySelector("#sec");
+
+
+let countDown = ()=>{
+  let futureDate = new Date("30 November 2024");
+  let currentDate = new Date();
+  let myDate = futureDate-currentDate;
+
+  let days = Math.floor(myDate/1000/60/60/24);
+  let hours = Math.floor(myDate/1000/60/60) % 24;
+  let mins = Math.floor(myDate/1000/60) % 60;
+  let secs = Math.floor(myDate/1000) % 60;
+
+  dayItem.innerHTML = days;
+  hourItem.innerHTML = hours;
+  minItem.innerHTML = mins;
+  secItem.innerHTML = secs;
+
+
+}
+
+countDown()
+
+setInterval(countDown, 1000)
+
+
+
+
 function navMenu() {
-    let navBar = document.querySelector(".navbar-sticky");
-    let scrollTopButton = document.querySelector("#scrollUp");
 
-    window.onscroll = function () {
-        var scroll = document.documentElement.scrollTop;
+  let scrollTopButton = document.querySelector("#scrollUp");
 
-        if (scroll >= 150) {
-            navBar.classList.add("navbar-sticky-moved-up")
-        } else {
-            navBar.classList.remove("navbar-sticky-moved-up")
-        }
+  window.onscroll = function () {
+      var scroll = document.documentElement.scrollTop;
 
-        if (scroll >= 250) {
-            navBar.classList.add("navbar-sticky-transitioned")
-            scrollTopButton.classList.add("scrollActive");
-        } else {
-            navBar.classList.remove("navbar-sticky-transitioned")
-            scrollTopButton.classList.remove("scrollActive");
-        }
+      if (scroll >= 250) {
+          scrollTopButton.classList.add("scrollActive");
+      } else {
 
-        
-        if (scroll >= 500) {
-            navBar.classList.add("navbar-sticky-on")
-        } else {
-            navBar.classList.remove("navbar-sticky-on")
-        }
+          scrollTopButton.classList.remove("scrollActive");
+      }
 
-
-
-    }
+  }
 }
 
 navMenu()
-
-
-
-
-// Counter
-function counter(id, start, end, duration) {
-    let obj = document.getElementById(id);
-
-    let current = start;
-    let range = end - start;
-    let increment = end > current ? 1 : -1;
-    let step = Math.abs(Math.floor(duration / range));
-
-    obj.innerText = current;
-    let timer = setInterval(() => {
-        current += increment;
-        obj.innerText = current;
-        if (current == end) {
-            clearInterval(timer)
-        }
-    }, step)
-}
-
-document.addEventListener("DOMContentLoaded", function () {
-
-
-    counter("count1", 0, 10, 1000);
-    counter("count2", 100, 20, 1000);
-    counter("count3", 0, 30, 1000);
-    counter("count4", 40, 15, 1000);
-
-});
-
-
-// Swipper JS ========
-var swiper = new Swiper('.swiper', {
-    loop: true,
-    centeredSlides: true,
-    centeredSlides: true,
-    effect: "coverflow",
-    grabCursor: true,
-    slidesPerView: "auto",
-    coverflowEffect: {
-        rotate: 50,
-        stretch: 0,
-        depth: 100,
-        loop: true,
-        modifier: 1,
-        slideShadows: true,
-    },
-
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
-    paginationClickable: true,
-
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-
-});
-
-
-
-
-
-
-
-
